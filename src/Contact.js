a6350b29e07aa680b94c7181b27ddd25f988909b
dharshini26693 <dharshini26693@gmail.com>
2024-08-28 08:19:09 +0530

    validation

--- src/Contact.js
+++ src/Contact.js
@@ -3,23 +3,19 @@ import {
   Button,
   Container,
   Form,
-  InputGroup,
   Row,
   Col,
   Navbar,
   Card,
-  Nav,
-  DropdownButton,
-  Dropdown,
+  Nav
 } from "react-bootstrap";
 
 const Contact = () => {
-  const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState("");
   const [phone, setPhone] = useState("");
-  const [countryCode, setCountryCode] = useState("+1");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [country, setCountry]=useState("")
+  const [zip, setZip] = useState("");
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -48,8 +44,11 @@ const Contact = () => {
     setEmail(e.target.value);
   };
 
-  const handleCountrySelect=(selectedCountry)=>{
-    setCountryCode(selectedCountry);
+  const handleZipChange = (e) => {
+    const re = /^\d{0,10}$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setZip(e.target.value);
+    }
   };
 
   return (
@@ -74,25 +73,24 @@ const Contact = () => {
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Row>
                     <Col md={6}>
-                    <Form.Group controlId="formCountry">
-                        <Form.Label> Click here to Select Year </Form.Label>
-                        <Form.Control
-                          as="select"
-                          value={countryCode}
-                          required
-                          onChange={(e) => setCountryCode(e.target.value)}
-                        >
-                          {/* <option value="+1">(United States)</option> */}
-                          <option value="+91">(India)</option>
-                          <option value="44">(United Kingdom)</option>
-                          <option value="61">(Australia)</option>
-                          <option value="+1">(Canada)</option>
-                        </Form.Control>
+                    <Form.Group controlId="formYear">
+                        <Form.Label> Select year </Form.Label>
+                        <select class="dropdown-year form-control" 
+                          id="select-year" aria-required="true" aria-invalid="true" 
+                          required name="sel_year" style={{ borderColor: "#CCCCCC", borderWidth: "1px", borderStyle: "solid" }}>
+                          <option value=""> Click here to select year</option>
+                          <option value="2001"> 2001 </option>
+                          <option value="2002"> 2002 </option>
+                          <option value="2003"> 2003 </option>
+                          <option value="2004"> 2004 </option>
+                          <option value="2005"> 2005 </option>
+                        </select> 
                         <Form.Control.Feedback type="invalid">
-                          Please select a country code.
+                          Please select year.
                         </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
+
                     <Col md={6}>
                     <Form.Group controlId="formName">
                         <Form.Label> Name </Form.Label>
@@ -107,30 +105,32 @@ const Contact = () => {
                           Please enter a valid name (letters only).
                         </Form.Control.Feedback>
                       </Form.Group>
-                    </Col>
+                    </Col>  
                   </Row>
 
                   <Row>
                     <Col md={6}>
-                      <Form.Group controlId="formName">
-                        <Form.Label> Name </Form.Label>
-                        <Form.Control
-                          required
-                          type="text"
-                          placeholder="Enter your name"
-                          value={name}
-                          onChange={handleNameChange}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          Please enter a valid name (letters only).
-                        </Form.Control.Feedback>
+                      <Form.Group controlId="formModel">
+                        <Form.Label> Model </Form.Label>
+                        <select class="dropdown-model form-control" 
+                          id="select-model" aria-required="true" aria-invalid="true" 
+                          required name="sel_model" style={{ borderColor: "#CCCCCC", borderWidth: "1px", borderStyle: "solid" }}>
+                          <option value=""> Model </option>
+                          <option value="Audi"> Audi </option>
+                          <option value="BMW"> BMW </option>
+                          <option value="Hyundai"> Hyundai </option>
+                          <option value="Ferrari"> Ferrari </option>
+                          <option value="Volvo"> Volvo </option>
+                        </select> 
+                          <Form.Control.Feedback type="invalid">
+                            Please select your model.
+                          </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
+
                     <Col md={6}>
                       <Form.Group controlId="formPhone">
                         <Form.Label> Phone Number </Form.Label>
-                        <InputGroup>
-                          <InputGroup.Text>{countryCode}</InputGroup.Text>
                           <Form.Control
                             required
                             type="text"
@@ -138,7 +138,6 @@ const Contact = () => {
                             value={phone}
                             onChange={handlePhoneChange}
                           />
-                        </InputGroup>
                         <Form.Control.Feedback type="invalid">
                           Please enter a valid phone number (numbers only).
                         </Form.Control.Feedback>
@@ -148,23 +147,21 @@ const Contact = () => {
 
                   <Row>
                     <Col md={6}>
-                      <Form.Group controlId="formCountry">
-                        <Form.Label> Country Code </Form.Label>
-                        <Form.Control
-                          as="select"
-                          value={countryCode}
-                          required
-                          onChange={(e) => setCountryCode(e.target.value)}
-                        >
-                          <option value="+1">(United States)</option>
-                          <option value="+91">(India)</option>
-                          <option value="44">(United Kingdom)</option>
-                          <option value="61">(Australia)</option>
-                          <option value="+1">(Canada)</option>
-                        </Form.Control>
-                        <Form.Control.Feedback type="invalid">
-                          Please select a country code.
-                        </Form.Control.Feedback>
+                    <Form.Group controlId="formMake">
+                        <Form.Label> Make </Form.Label>
+                        <select class="dropdown-make form-control" 
+                          id="select-make" aria-required="true" aria-invalid="true" 
+                          required name="sel_make" style={{ borderColor: "#CCCCCC", borderWidth: "1px", borderStyle: "solid" }}>
+                          <option value=""> Make </option>
+                          <option value="A3"> A3 </option>
+                          <option value="Accent"> Accent </option>
+                          <option value="320"> 320 </option>
+                          <option value="Volvo 140"> Volvo 140</option>
+                          <option value="166 Inter"> 166 Inter </option>
+                        </select> 
+                          <Form.Control.Feedback type="invalid">
+                            Please select your make.
+                          </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
 
@@ -188,64 +185,37 @@ const Contact = () => {
 
                   <Row>
                     <Col md={6}>
-                      <Form.Group controlId="formGender">
-                        <Form.Label> Gender </Form.Label>
-                        <div>
-                          <Form.Check
-                            required
-                            type="radio"
-                            label="Male"
-                            name="gender"
-                            id="male"
-                          />
-                          <Form.Check
-                            required
-                            type="radio"
-                            label="Female"
-                            name="gender"
-                            id="female"
-                          />
-                          <Form.Check
-                            required
-                            type="radio"
-                            label="Others"
-                            name="gender"
-                            id="others"
-                          />
-                        </div>
-                        <Form.Control.Feedback type="invalid">
-                          Please select your gender.
-                        </Form.Control.Feedback>
+                    <Form.Group controlId="formPart">
+                        <Form.Label> Part </Form.Label>
+                        <select class="dropdown-part form-control" 
+                          id="select-part" aria-required="true" aria-invalid="true" 
+                          required name="sel_part" style={{ borderColor: "#CCCCCC", borderWidth: "1px", borderStyle: "solid" }}>
+                          <option value=""> Part </option>
+                          <option value="2001"> Engine </option>
+                          <option value="2002"> Airbag </option>
+                          <option value="2003"> Turbo </option>
+                          <option value="2004"> Headlight </option>
+                          <option value="2005"> Side mirror </option>
+                        </select> 
+                          <Form.Control.Feedback type="invalid">
+                            Please select your part.
+                          </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
 
                     <Col md={6}>
-                      <Form.Group controlId="formCountry">
-                        <Form.Label>Country</Form.Label>
-                        <InputGroup>
-                          <DropdownButton
-                            variant="outline=secondary"
-                            id="dropdown-basic-button"
-                            title={country || "Choose..."}
-                            onSelect={handleCountrySelect}>
-                            <Dropdown.Item eventKey="United States"> United States </Dropdown.Item>
-                            <Dropdown.Item eventKey="India"> India </Dropdown.Item>
-                            <Dropdown.Item eventKey="Canada"> Canada </Dropdown.Item>
-                            <Dropdown.Item eventKey="Australia"> Australia </Dropdown.Item>
-                            <Dropdown.Item eventKey="United Kingdom"> United Kingdom </Dropdown.Item>
-                          </DropdownButton>
+                    <Form.Group controlId="formPhone">
+                        <Form.Label> Enter ZIP </Form.Label>
                           <Form.Control
-                            type="text"
-                            value={country}
-                            onChange={(e)=>setCountry(e.target.value)}
                             required
-                            isInvalid={!country && validated}
-                            readOnly
+                            type="text"
+                            placeholder="Enter your phone number"
+                            value={zip}
+                            onChange={handleZipChange}
                           />
-                          <Form.Control.Feedback type="invalid">
-                            Please select your country.
-                          </Form.Control.Feedback>
-                        </InputGroup>
+                        <Form.Control.Feedback type="invalid">
+                          Please enter a valid ZIP (numbers only).
+                        </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
                   </Row>
